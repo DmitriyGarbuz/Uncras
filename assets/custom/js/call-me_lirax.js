4b31b61b39e48bf57630b47ecee70330dcf0e4af
DmitriyGarbuz <51447305+DmitriyGarbuz@users.noreply.github.com>
2021-04-09 21:53:45 +0300

    uncras version 1.0

---
+++ assets/custom/js/call-me_lirax.js
@@ -0,0 +1,12 @@
+// BEGIN CALL2ME CODE {literal} 
+(function() {
+var widgetId = 'c94816e6431857987c1f35899f3d1b9f';
+var s = document.createElement('script');
+s.type = 'text/javascript';
+s.charset = 'utf-8';
+s.async = true;
+s.src = '//callme.voip.com.ua/script/widget/'+widgetId;
+var ss = document.getElementsByTagName('script')[0];
+ss.parentNode.insertBefore(s, ss);}
+)();
+// {/literal} END CALL2ME CODE
---
+++ assets/custom/js/heder_mob.js
@@ -0,0 +1,37 @@
+
+// var btn = document.querySelector('.btn-white').querySelectorAll("a");
+
+// console.log(btn[0]);
+
+// function closeHeder() {
+//     var heder = document.getElementById('menu-12');
+//     heder.style.display = "none";
+//     console.log(heder.style.display);
+//     var closer = document.querySelector('.appointer_widget-close-button');
+
+// };
+
+// function openHeder() {
+//     var heder = document.getElementById('menu-12');
+//     heder.style.display = "block";
+//     console.log(heder.style.display);
+
+// };
+
+// closer.addEventListener("click", openHeder);
+// btn[0].addEventListener("click", function () {
+//     var heder = document.getElementById('menu-12');
+//     heder.style.display = "none";
+//     console.log(heder.style.display);
+// });
+
+
+// function cliked(event) {
+//     console.log(event.target);
+// }
+
+// var a = document.addEventListener("click", (e) => {
+//     console.log(e.target.value);
+// });
+
+
